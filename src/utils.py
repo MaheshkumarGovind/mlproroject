461326535b5d6c8b27694681b6fe7f298e3e2757
Mahesh Kumar <Maheshgovind446@gmail.com>
2025-11-06 13:48:49 +0530

    model trainer

---
+++ src/utils.py
@@ -0,0 +1,67 @@
+import os
+import sys
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
+from src.exception import CustomException
+
+
+def save_object(file_path, obj):
+    """
+    Save a Python object using pickle.
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    """
+    Train and evaluate multiple ML models using GridSearchCV.
+    Returns a report with the test R² scores.
+    """
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+            params = param[model_name]
+
+            gs = GridSearchCV(model, params, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    """
+    Load a pickled Python object.
+    """
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
